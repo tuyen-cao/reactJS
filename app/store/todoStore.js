22f8f6456f78891e7e026c66b2cadb89848bb061
CAO KIM TUYEN <tuyen.cao@pyramid-consulting.com>
2016-07-16 22:36:51 +0700

    finished step 2-1

--- app/store/todoStore.js
+++ app/store/todoStore.js
@@ -2,9 +2,16 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from '../reducers';
 
 
-const logger = (store) => (next) => (action) => {
-    console.log('dispatching:', action);
-  	return next(action);
+const logger = (store) => (dispatch) => (action) => {
+    /*console.log('dispatching:', action);
+    console.log('next state:', store.getState());
+  	return next(action);*/
+
+	console.log('Dispatching:', action.type)
+	console.log('Old state:', store.getState())
+	var result = dispatch(action)
+	console.log('New state:', store.getState())
+	return result
 };
 
 const todoStore = createStore(
